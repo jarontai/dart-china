a0240bb9668df5dd339d28c96e2e534824f14aa5
daiqiu <daiqiu@wohua.com.cn>
2021-05-18 19:43:07 +0800

    Updates

--- dart_china/lib/features/auth/view/login_page.dart
+++ dart_china/lib/features/auth/view/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:dart_china/commons.dart';
+import 'package:dart_china/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -14,45 +15,67 @@ class LoginPage extends StatelessWidget {
         child: SafeArea(
           child: Column(
             children: [
-              Expanded(
-                flex: 3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 35,
-                      child: Image.asset(
-                        'assets/icon/logo_dart.png',
-                        height: 50,
-                        width: 50,
-                      ),
-                    ),
-                    SizedBox(height: 40),
-                    Text(
-                      '欢迎回到 DartChina',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                  flex: 7,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(20),
-                        topLeft: Radius.circular(20),
-                      ),
-                    ),
-                  )),
+              buildHead(),
+              buildForm(),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Expanded buildForm() {
+    return Expanded(
+        flex: 7,
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20),
+              topLeft: Radius.circular(20),
+            ),
+          ),
+          child: Column(
+            children: [
+              InputWidget(
+                label: '用户名',
+              ),
+              InputWidget(
+                label: '密码',
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Expanded buildHead() {
+    return Expanded(
+      flex: 3,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 30,
+            child: Image.asset(
+              'assets/icon/logo_dart.png',
+              height: 45,
+              width: 45,
+            ),
+          ),
+          SizedBox(height: 40),
+          Text(
+            '欢迎回到 DartChina',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+        ],
       ),
     );
   }
--- dart_china/lib/widgets/input_widget.dart
+++ dart_china/lib/widgets/input_widget.dart
@@ -5,21 +5,35 @@ class InputWidget extends StatelessWidget {
   const InputWidget({
     Key? key,
     this.hint = '',
+    this.label = '',
   }) : super(key: key);
 
   final String hint;
+  final String label;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: hint,
-          border: OutlineInputBorder(),
-          hintStyle: TextStyle(color: ColorPalette.postTextColor),
-          focusedBorder: OutlineInputBorder(),
-          fillColor: ColorPalette.tagColr,
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(label),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+              hintText: hint,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              hintStyle: TextStyle(color: ColorPalette.postTextColor),
+              focusedBorder: OutlineInputBorder(),
+              fillColor: ColorPalette.tagColr,
+            ),
+          ),
+        ],
       ),
     );
   }
