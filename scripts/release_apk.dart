26292c3c1fc6a3acff7c9720153777c11a8675fa
daiqiu <daiqiu@wohua.com.cn>
2021-06-07 16:12:07 +0800

    Add scripts

---
+++ scripts/release_apk.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:yaml/yaml.dart';
+import 'package:path/path.dart' as path;
+
+const appName = 'dart_china';
+const version = 'version';
+const apkPath = 'build/app/outputs/flutter-apk/app-armeabi-v7a-release.apk';
+
+main(List<String> args) async {
+  final parser = ArgParser()
+    ..addOption(version, abbr: 'v', help: 'version num', valueHelp: 'VERSION');
+  final argResults = parser.parse(args);
+
+  final dir = Directory(path.join(Directory.current.path, appName));
+  final pubspec = File(path.join(dir.path, 'pubspec.yaml'));
+  print(dir.path);
+  print(pubspec.path);
+
+  if (dir.existsSync() && pubspec.existsSync()) {
+    final yaml = pubspec.readAsStringSync();
+    final doc = loadYaml(yaml);
+    final version = doc['version'];
+    if (version != null) {
+      final process = await Process.start(
+          'flutter', ['build', 'apk', '--split-per-abi'],
+          workingDirectory: dir.path);
+      process.stdout.listen((event) {
+        print(utf8.decode(event));
+      }, onDone: () async {
+        final apkFile = File(path.join(dir.path, apkPath));
+        if (apkFile.existsSync()) {
+          final finalApk =
+              apkFile.path.replaceAll('app-', '$appName-$version-');
+          await apkFile.copySync(finalApk);
+          print('Finish build: $finalApk');
+        }
+      });
+      process.stderr.listen((event) {
+        print(utf8.decode(event));
+      });
+    } else {
+      print('Pubspec file invalid');
+    }
+  }
+}
